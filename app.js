ff71b32bcb6419ce4791ecafda4a4534bc761be1
carlostaborda1987-boop <carlostaborda1987@gmail.com>
2026-09-10 07:50:39 +0200

    cambios en el repositorio

--- app.js
+++ app.js
@@ -5,27 +5,33 @@ const PORT = 3000;
 
 app.get('/', (req, res) => {
     res.json({
-    system: 'PHANTOM-01',
-    status: 'ONLINE',
-    environment: 'SIMULATION',
-    version: '1.0.0'
+        system: 'PHANTOM-01',
+        status: 'ONLINE',
+        environment: 'SIMULATION',
+        version: '1.0.0'
     });
 });
 
 app.get('/objetivos', (req, res) => {
     res.json([
-    {
-    name: 'Pedro Sánchez',
-    codename: 'OBJ-001',
-    status: 'MONITORED',
-    riskLevel: 'HIGH'
-    },
-    {
-    name: 'Isabel Díaz Ayuso',
-    codename: 'OBJ-002',
-    status: 'MONITORED',
-    riskLevel: 'HIGH'
-    }
+        {
+            name: 'Pedro Sánchez',
+            codename: 'OBJ-001',
+            status: 'MONITORED',
+            riskLevel: 'HIGH'
+        },
+        {
+            name: 'Isabel Díaz Ayuso',
+            codename: 'OBJ-002',
+            status: 'MONITORED',
+            riskLevel: 'HIGH'
+        },
+        {
+            name: 'Objetivo 083-e01',
+            codename: '083-e01',
+            status: 'MONITORED',
+            riskLevel: 'CRITICAL'
+        }
     ]);
 });
 
